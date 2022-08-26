cd94673d996ef0752e6727f322f4827cb1be7390
Marcellino <halolino22@gmail.com>
2022-08-26 16:48:39 +0700

    test

--- src/pages/Favorite/Favorite.js
+++ src/pages/Favorite/Favorite.js
@@ -1,11 +1,67 @@
-import React from 'react'
-import Navbar from '../../components/Navbar'
+import React, { useEffect, useState } from 'react';
+import Navbar from '../../components/Navbar';
+import axios from 'axios';
 
 export default function Favorite() {
+  const [favorite, setFavorite] = useState([]);
+
+  const getFavorite = () => {
+    fetch('favorite.json', {
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setFavorite(data);
+      });
+  };
+
+  useEffect(() => {
+    getFavorite();
+  }, []);
+
   return (
     <>
       <Navbar />
-      <h1>Favorite</h1>
+      <div className="container">
+        <h1>Favorite</h1>
+        <div className="row">
+          {favorite &&
+            favorite.length > 0 &&
+            favorite.map((datas) => {
+              return (
+                <div
+                  style={{
+                    width: '15rem',
+                    margin: '10px',
+                  }}
+                  key={datas.id}
+                >
+                  <img
+                    className="card-img-top"
+                    src="https://images.unsplash.com/photo-1611339555312-e607c8352fd7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+                    style={{
+                      borderRadius: '10px',
+                    }}
+                  />
+                  <div
+                    className="card border-light mb-3"
+                    style={{ maxWidth: '18rem' }}
+                  >
+                    <div className="card-body">
+                      <h5 className="card-title">{datas.title}</h5>
+                      <p className="card-text">{datas.artist}</p>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+        </div>
+      </div>
     </>
-  )
+  );
 }
--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,10 +1,23 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Navbar from '../../components/Navbar';
+import axios from 'axios';
+
+const baseUrl = 'https://mocki.io/v1/d933391c-4422-4e65-9693-c221260cca18';
 
 export default function Homepage() {
   const [homeplay, setHomeplay] = useState([]);
+  const [category, setCategory] = useState([]);
 
+  const getCategory = () => {
+    axios
+      .get(`${baseUrl}`)
+      .then((response) => {
+        setCategory(response.data.kategori);
+        console.log(response.data.kategori);
+      })
+      .catch((error) => console.error(`error: ${error}`));
+  };
   const getData = () => {
     fetch('homeplay.json', {
       headers: {
@@ -12,48 +25,35 @@ export default function Homepage() {
         Accept: 'application/json',
       },
     })
-      .then(function (response) {
+      .then((response) => {
         return response.json();
       })
-      .then(function (data) {
+      .then((data) => {
         setHomeplay(data);
       });
   };
 
   useEffect(() => {
     getData();
+    getCategory();
   }, []);
 
   return (
-    <>
+    <div>
       <Navbar />
-      <h1 style={{ paddingLeft: '55px' }}>PlayList</h1>
       <div className="container">
+        <h1>Playlist</h1>
         <div className="row">
           {homeplay &&
             homeplay.length > 0 &&
             homeplay.map((datas) => {
               return (
-                // <div
-                //   className="card"
-                //   style={{ width: '15rem', margin: '10px' }}
-                //   key={datas.id}
-                // >
-                //   <img
-                //     className="card-img-top"
-                //     src="https://images.unsplash.com/photo-1611339555312-e607c8352fd7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
-                //     alt="Card Image Cap"
-                //   />
-                //   <div className="card-body">
-                //     <h5 className="card-title">{datas.title}</h5>
-                //     <p className="card-text">{datas.artist}</p>
-                //   </div>
-                // </div>
                 <div
                   style={{
                     width: '15rem',
                     margin: '10px',
                   }}
+                  key={datas.id}
                 >
                   <img
                     className="card-img-top"
@@ -75,7 +75,12 @@ export default function Homepage() {
               );
             })}
         </div>
+        <div>
+          {category.map((data) => {
+            return <p>{data}</p>;
+          })}
+        </div>
       </div>
-    </>
+    </div>
   );
 }
